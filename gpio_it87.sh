1c8296f4ab1c320f39e9f6fc070cb44916540a00
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2016-12-11 11:44:32 +0100

    dynamically find the GPIO base for the gpio_it87 module
    This also allows us to detect when the gpio_it87 module is not loaded so
    we can print an error message accordingly.

--- gpio_it87.sh
+++ gpio_it87.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
+gpio_it87_get_base() {
+	local IT87_GPIO_LABEL_FILE=$(grep -l "gpio_it87" /sys/class/gpio/gpiochip*/label)
+
+	if [ -z "${IT87_GPIO_LABEL_FILE}" ]
+	then
+		echo "could not find gpio_it87 GPIO entry"
+		echo "make sure the gpio_it87 driver is supported by your kernel and loaded"
+		exit 1
+	fi
+
+	cat "$(dirname "${IT87_GPIO_LABEL_FILE}")/base"
+}
+
 gpio_it87_set_output_value() {
 	local GPIO_BANK_NUM=${1}
 	local GPIO_BANK_PIN=${2}
 	local GPIO_VALUE=${3}
 	local GPIO_PIN_DIR="/sys/class/gpio/it87_gp${GPIO_BANK_NUM}${GPIO_BANK_PIN}"
-	local IT87_GPIO_BASE=448
+	local IT87_GPIO_BASE=$(gpio_it87_get_base)
 	local GLOBAL_GPIO_NUM=$(( IT87_GPIO_BASE + ((GPIO_BANK_NUM - 1) * 8) + $GPIO_BANK_PIN ))
 
 	if [ ! -e "${GPIO_PIN_DIR}" ]
