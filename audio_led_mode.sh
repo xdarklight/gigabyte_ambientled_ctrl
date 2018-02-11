9cde36d0ca4641657cf38f754a14fe06b350b8c0
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2018-02-11 22:20:55 +0100

    report an error in audio_led_mode.sh if the board is not supported
    Instead of guessing that an unuspported board supports the "advanced"
    features this will now simply show an error indicating that the board is
    not supported (yet).

--- audio_led_mode.sh
+++ audio_led_mode.sh
@@ -55,14 +55,19 @@ config_advanced_audio_led_board() {
 	esac
 }
 
-case "$(cat /sys/class/dmi/id/board_name)" in
+BOARD_NAME="$(cat /sys/class/dmi/id/board_name)"
+
+case "${BOARD_NAME}" in
 	"H170M-D3H")
 		config_simple_audio_led_board "${1}"
 		;;
 	"H170-Gaming 3" |\
 	"Z170X-Gaming 7" |\
-	"Z170X-UD5 TH-CF" |\
-	*)
+	"Z170X-UD5 TH-CF")
 		config_advanced_audio_led_board "${1}"
+		;;
+	*)
+		echo "Unuspported board: \"${BOARD_NAME}\"!"
+		exit 1
 		;;
 esac
